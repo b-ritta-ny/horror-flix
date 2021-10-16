d3eaa2604e64c029858adae1106dc3e5ee14048b
Joann Pan <joannlpan@gmail.com>
2021-10-16 18:18:24 -0400

    add strong params and before action for user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,20 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+    before_action :find_user
+
+
+#GET /users
+    def index
+        @users = Users.all
+        render json: @user
+    end
 end
+
+
+private 
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :username, :password_digest)
+    end    
