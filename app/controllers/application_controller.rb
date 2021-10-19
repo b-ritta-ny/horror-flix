da67c70c3a0864912ed099de60edd5f441cacf13
Joann Pan <joannlpan@gmail.com>
2021-10-19 16:17:42 -0400

    add current user method for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+    helpers do 
+
+        def current_user 
+            User.first
+        end    
+
+        #currently mocked version 
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
