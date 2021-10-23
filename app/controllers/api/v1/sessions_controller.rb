172b6dcb8550f1d3569f219d036d7db9051b01fc
Joann Pan <joannlpan@gmail.com>
2021-10-23 13:56:59 -0400

    change get to post

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create
-    end 
+        byebug
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
+  post "api/v1/login", to: "session#create"
+
   resources :horror_movie_genres
   namespace :api do
     namespace :v1 do
       resources :genres
       resources :horror_movies
       resources :users
-      resources :sessions
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
