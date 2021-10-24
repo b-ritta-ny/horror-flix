e881a1fd5b577ada7e188e766d8b51c191f761b9
Joann Pan <joannlpan@gmail.com>
2021-10-24 17:46:33 -0400

    change alert back to error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -14,12 +14,11 @@ class Api::V1::SessionsController < ApplicationController
     end
 
     def get_current_user
-        binding.pry
         if logged_in?
             render json: current_user
         else
             render json: {
-                alert: "No one logged in."
+                error: "No one logged in."
             }
         end
     end
