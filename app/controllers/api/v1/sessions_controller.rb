7c8de15d461ce8b88c05493973ea447d997df4af
Joann Pan <joannlpan@gmail.com>
2021-10-25 15:24:01 -0400

    add comma

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -25,6 +25,9 @@ class Api::V1::SessionsController < ApplicationController
 
     def destroy
         session.clear
+        render json: {
+            notice: "Successfully logged out."
+        }
     end
 
 end
