1bd772645a7ad4634090b8bd5d1fc1446feacd44
Joann Pan <joannlpan@gmail.com>
2021-10-30 14:46:12 -0400

    add create action in users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,21 @@ class Api::V1::UsersController < ApplicationController
     end
 end
 
+#POST /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            render json: UserSerializer.new(@user)
+            binding.pry
+        else
+            resp = {
+                error: @user.errors.full_messages.to_sentence
+            }
+            render json: resp, status: :unprocessable_entity
+        end
+    end
+
 private 
     def find_user
         @user = User.find(params[:id])
