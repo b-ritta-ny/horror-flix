d54c6a05ded7421ec50b2d3132b7c7b0166354c5
Joann Pan <joannlpan@gmail.com>
2021-10-30 16:43:50 -0400

    add route to signup create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,11 +10,12 @@ end
 
 #POST /users
     def create
+        binding.pry
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
             render json: UserSerializer.new(@user)
-            binding.pry
+           
         else
             resp = {
                 error: @user.errors.full_messages.to_sentence
