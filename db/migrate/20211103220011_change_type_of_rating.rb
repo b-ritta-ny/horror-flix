7dddf780df63c72447984081acc094beffe5fc62
Joann Pan <joannlpan@gmail.com>
2021-11-03 18:11:12 -0400

    edit rating column in reviews table

--- db/migrate/20211103220011_change_type_of_rating.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTypeOfRating < ActiveRecord::Migration[6.1]
-  def change
-    change_column :reviews, :rating, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_204740) do
+ActiveRecord::Schema.define(version: 2021_11_03_220848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2021_11_03_204740) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "description"
-    t.string "rating"
+    t.integer "rating"
     t.bigint "user_id"
     t.bigint "horror_movie_id"
     t.datetime "created_at", precision: 6, null: false
