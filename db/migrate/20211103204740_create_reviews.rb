df215dc67873095f852595b82285d622b45bfca0
Joann Pan <joannlpan@gmail.com>
2021-11-03 17:41:54 -0400

    add review migration

--- db/migrate/20211103204740_create_reviews.rb
+++ db/migrate/20211103204740_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
+      t.string :description
+      t.string :rating
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :horror_movie, foreign_key: true
 
       t.timestamps
     end
