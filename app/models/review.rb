0df4f0f9d03f3b04504141953a8104a8b272a793
Joann Pan <joannlpan@gmail.com>
2021-11-03 17:36:10 -0400

    add relationships to review and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    belongs_to :user
+    belongs_to :horror_movie
+
+    validates :description, :rating
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :reviews 
-    has_many :horror_movies, through: :reviews 
+    has_many :horror_movies, through: :reviews
 
-
+    validates :name, :password, :username
 end
