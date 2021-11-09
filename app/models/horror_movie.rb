cca135f2ab3c06b3b23de585ea0a918cca770ace
Joann Pan <joannlpan@gmail.com>
2021-11-09 13:40:07 -0500

    add slugify and avg_score method to horror movies model

--- app/models/horror_movie.rb
+++ app/models/horror_movie.rb
@@ -6,6 +6,17 @@ class HorrorMovie < ApplicationRecord
     has_many :users, through: :reviews 
 
     validates :title, :director, presence: true
+
+    before_create :slugify
+
+    def slugify
+        self.slug = title.parameterize
+    end
+
+    def avg_score
+        self.reviews.average(:rating).round(2).to_f
+    end
+
 end
 
 
