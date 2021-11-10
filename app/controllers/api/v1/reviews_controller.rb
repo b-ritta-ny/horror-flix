4ce5249f21b75b0ea3bb2a3c0151ddc697000c92
Joann Pan <joannlpan@gmail.com>
2021-11-10 14:34:16 -0500

    add review index action and strong params

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,11 +2,18 @@ class Api::V1::ReviewsController < ApplicationController
 
 #POST /reviews
     def index
+        @review = Review.new(review_params)
+
+        if @review.save
+            render json: ReviewSerializer.new(@review)
+        else
+            render json: { error: review.errors.messages }, status: 422
+        end
     end
 
     private
 
     def review_params 
-        params.require(:review).permit(:title, :description, :score, :airline_id)
+        params.require(:review).permit(:title, :description, :score, :horror_movie_id, :user_id)
     end
 end
