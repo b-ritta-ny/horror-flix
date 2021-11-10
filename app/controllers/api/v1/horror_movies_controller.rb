0765458dde976ee69e9f9aa6e49fc767d69e7a80
Joann Pan <joannlpan@gmail.com>
2021-11-10 17:29:15 -0500

    include options of reviews in horror movies serializer

--- app/controllers/api/v1/horror_movies_controller.rb
+++ app/controllers/api/v1/horror_movies_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::HorrorMoviesController < ApplicationController
     def index
         if logged_in?
             @horror_movies = current_user.horror_movies
-            render json: HorrorMovieSerializer.new(@horror_movies)
+            render json: HorrorMovieSerializer.new(@horror_movies, options)
         elsif 
             @horror_movies = HorrorMovie.all
-            render json: HorrorMovieSerializer.new(@horror_movies)
+            render json: HorrorMovieSerializer.new(@horror_movies, options)
         else
             render json: {
                 error: "You must be logged in to see your movies."
@@ -18,6 +18,13 @@ class Api::V1::HorrorMoviesController < ApplicationController
 
     #LATER: have to add a condition where if user isn't logged in, then display all the movies in the database with average score
     #also need to add route that isn't nested under user module 
+
+    private
+
+    # Used For compound documents with fast_jsonapi
+    def options
+        @options ||= { include: %i[reviews] }
+    end
 
 
 end
--- app/serializers/horror_movie_serializer.rb
+++ app/serializers/horror_movie_serializer.rb
@@ -2,4 +2,9 @@ class HorrorMovieSerializer
   include JSONAPI::Serializer
   attributes :poster, :title, :director, :year_released
 
+  has_many :reviews
+
+  attribute :average_score do |object|
+    (object.avg_score)
+  end
 end
