ae826db5a5c3078f244ec53b8e37a4f30fb44369
Joann Pan <joannlpan@gmail.com>
2021-11-11 11:25:53 -0500

    attempt to edit migration to support rollback

--- db/migrate/20211103220848_change_rating_type_to_integer.rb
+++ db/migrate/20211103220848_change_rating_type_to_integer.rb
@@ -1,5 +1,12 @@
 class ChangeRatingTypeToInteger < ActiveRecord::Migration[6.1]
   def change
-    change_column :reviews, :rating, 'integer USING CAST(rating AS integer)'
+
+    def up
+      change_column :reviews, :rating, 'integer USING CAST(rating AS integer)'
+    end
+
+    def down
+      change_column :reviews, :rating, :string
+    end
   end
 end
