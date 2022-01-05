e1f27ea99b90f0843983bca05771cd9740d415e6
Joann Pan <joannlpan@gmail.com>
2022-01-05 17:27:39 -0500

    change rating datatype to bigint to try and solve error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_183008) do
+ActiveRecord::Schema.define(version: 2022_01_05_222226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_12_11_183008) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "description"
-    t.integer "rating"
+    t.bigint "rating"
     t.bigint "user_id"
     t.bigint "horror_movie_id"
     t.datetime "created_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,8 @@ horror_movie3 = HorrorMovie.create(poster: "https://amc-theatres-res.cloudinary.
                 title: "The Witch", director: "Robert Eggers")
 horror_movie4 = HorrorMovie.create(poster: "https://amc-theatres-res.cloudinary.com/v1594123072/amc-cdn/production/2/movies/44400/44369/Poster/p_800x1200_AMC_Oculus_07052020.jpg",
                 title: "Midsommar", director: "Ari Aster")
+horror_movie5 = HorrorMovie.create(poster: "https://www.uphe.com/sites/default/files/styles/scale__203w_/public/2021/09/Old_poster.webp?itok=FJOcXWKi",
+                title: "Old", director: "M. Night Shyamalan")
 
 #horror movie genres
 h1 = HorrorMovieGenre.create(horror_movie_id: horror_movie1.id, genre_id: genre1.id)
@@ -42,11 +44,12 @@ h3 = HorrorMovieGenre.create(horror_movie_id: horror_movie1.id, genre_id: genre4
 h4 = HorrorMovieGenre.create(horror_movie_id: horror_movie1.id, genre_id: genre2.id)
 
 #reviews 
-r1 = Review.create(description: "Terrifying!", title: "Terrifying movie", rating: 3, user_id: user3.id, horror_movie_id: horror_movie1.id)
-r2 = Review.create(description: "Scary and original", title: "Great movie", rating: 5, user_id: user2.id, horror_movie_id: horror_movie1.id)
-r3 = Review.create(description: "I was on the edge of my seat", title: "Loved this movie!", rating: 4, user_id: user1.id, horror_movie_id: horror_movie1.id)
+r1 = Review.create(description: "Terrifying!", title: "Terrifying movie", rating: 3, user_id: 3, horror_movie_id: 1)
+r2 = Review.create(description: "Scary and original", title: "Great movie", rating: 5, user_id: 2, horror_movie_id: 1)
+r3 = Review.create(description: "I was on the edge of my seat", title: "Loved this movie!", rating: 4, user_id: 1, horror_movie_id: 1)
 
-r3 = Review.create(description: "I loved it!", title: "Loved this movie!", rating: 5, user_id: user3.id, horror_movie_id: horror_movie2.id)
-r4 = Review.create(description: "I'd recommend it to everyone.", title: "great movie!", rating: 4, user_id: user2.id, horror_movie_id: horror_movie2.id)
-r6 = Review.create(description: "Super original", title: "great", rating: 4, user_id: user1.id, horror_movie_id: horror_movie2.id)
+r3 = Review.create(description: "I loved it!", title: "Loved this movie!", rating: 5, user_id: 3, horror_movie_id: 2)
+r4 = Review.create(description: "I'd recommend it to everyone.", title: "great movie!", rating: 4, user_id: 2, horror_movie_id: 2)
+r6 = Review.create(description: "Super original", title: "great", rating: 4, user_id: 1, horror_movie_id: 2)
 
+r7 = Review.create(description: "Super original plot and great execution", title: "Fantastic watch", rating: 4, user_id: 1, horror_movie_id: 5)
