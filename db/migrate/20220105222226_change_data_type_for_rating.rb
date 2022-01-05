875517a569401b9d8c63347260d578feab4cc0fe
Joann Pan <joannlpan@gmail.com>
2022-01-05 17:44:27 -0500

    add USING to change column migration

--- db/migrate/20220105222226_change_data_type_for_rating.rb
+++ db/migrate/20220105222226_change_data_type_for_rating.rb
@@ -1,5 +1,8 @@
 class ChangeDataTypeForRating < ActiveRecord::Migration[7.0]
   def change
-    change_column :reviews, :rating, :bigint
+    change_column :reviews, :rating, 'bigint USING CAST(rating AS bigint)'
   end
 end
+
+
+
