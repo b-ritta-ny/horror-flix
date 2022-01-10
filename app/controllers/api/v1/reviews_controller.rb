496845de0cf3f51d856381c18416dd5162e64ba0
Joann Pan <joannlpan@gmail.com>
2022-01-10 16:35:15 -0500

    remove byebug

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::ReviewsController < ApplicationController
 
 #POST /reviews
     def create
-        byebug
         @review = horror_movie.reviews.new(review_params)
     
         if @review.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_222226) do
+ActiveRecord::Schema.define(version: 2022_01_09_204440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_01_05_222226) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.string "name"
     t.index ["horror_movie_id"], name: "index_reviews_on_horror_movie_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
