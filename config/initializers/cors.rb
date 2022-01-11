69a34e2ce3991e8ba8942e61192c1f2d6533278c
Joann Pan <joannlpan@gmail.com>
2022-01-11 18:55:30 -0500

    update cors to follow requests from netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://horror-movie-frontend.netlify.app/'
 
     resource '*',
       headers: :any,
