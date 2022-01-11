a66a6515af3d19c616ded2cb62c648a340018cb7
Joann Pan <joannlpan@gmail.com>
2022-01-11 17:33:43 -0500

    add rmovie release date to seed file to avoid null on frontend

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,15 +27,15 @@ genre6 = Genre.create(name: "summer festival")
 
 #horror movie
 horror_movie1 = HorrorMovie.create(poster: "https://amc-theatres-res.cloudinary.com/v1594123072/amc-cdn/production/2/movies/44400/44369/Poster/p_800x1200_AMC_Oculus_07052020.jpg",
-                title: "Oculus", director: "Mike Flanagan")
+                title: "Oculus", year_released: '2013-04-03', director: "Mike Flanagan")
 horror_movie2 = HorrorMovie.create(poster: "https://m.media-amazon.com/images/M/MV5BOTU5MDg3OGItZWQ1Ny00ZGVmLTg2YTUtMzBkYzQ1YWIwZjlhXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_.jpg",
-                title: "Hereditary", director: "Ari Aster")
-horror_movie3 = HorrorMovie.create(poster: "https://amc-theatres-res.cloudinary.com/v1594123072/amc-cdn/production/2/movies/44400/44369/Poster/p_800x1200_AMC_Oculus_07052020.jpg",
-                title: "The Witch", director: "Robert Eggers")
-horror_movie4 = HorrorMovie.create(poster: "https://amc-theatres-res.cloudinary.com/v1594123072/amc-cdn/production/2/movies/44400/44369/Poster/p_800x1200_AMC_Oculus_07052020.jpg",
-                title: "Midsommar", director: "Ari Aster")
+                title: "Hereditary", year_released: '2018-06-08', director: "Ari Aster")
+horror_movie3 = HorrorMovie.create(poster: "https://www.themoviedb.org/t/p/w1280/zap5hpFCWSvdWSuPGAQyjUv2wAC.jpg",
+                title: "The Witch", year_released: '2016-02-19', director: "Robert Eggers")
+horror_movie4 = HorrorMovie.create(poster: "https://www.themoviedb.org/t/p/w1280/eycO6M2s38xO1888Gq2gSrXf0A6.jpg",
+                title: "Midsommar", year_released: '2019-07-03', director: "Ari Aster")
 horror_movie5 = HorrorMovie.create(poster: "https://www.uphe.com/sites/default/files/styles/scale__203w_/public/2021/09/Old_poster.webp?itok=FJOcXWKi",
-                title: "Old", director: "M. Night Shyamalan")
+                title: "Old", year_released: '2020-12-21', director: "M. Night Shyamalan")
 
 #horror movie genres
 h1 = HorrorMovieGenre.create(horror_movie_id: horror_movie1.id, genre_id: genre1.id)
