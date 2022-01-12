f8565099895be5a8a5e4a114348412eef35d7ae2
Joann Pan <joannlpan@gmail.com>
2022-01-12 10:58:06 -0500

    attempt to unblock cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://scarymovies.netlify.app/'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://scarymovies.netlify.app'
 
     resource '*',
       headers: :any,
